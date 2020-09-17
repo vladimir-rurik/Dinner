991430512960a1fe9b9ed8bb0dbd8f443045bfee
Vladimi Rurik <vladimir.rurik@gmail.com>
2020-09-18 01:15:17 +0300

    finished the basics

--- Dinner/Program.cs
+++ Dinner/Program.cs
@@ -7,18 +7,26 @@ namespace Dinner
 	{
 		static void Main(string[] args)
 		{
-			(string, string)[] table = new (string, string)[10];
-			table[0] = ("S", "hr. Sepp");
-			table[7] = ("S", "pr. Sepp");
+			(string, string)[] table = new (string, string)[14];
+			table[0] = ("SP", "hr. Sepp"); // ei meeldi pr. Elkin
+			table[7] = ("SH", "pr. Sepp"); // ei meeldi hr. Bertini
 
 			List<(string, string)> guests = new List<(string, string)>()
 			{
-				("E", "Anni Elkin"), // armunud
-				("S", "Kalle Sepp"), // armunud
-				("HR", "Jenny Harner"), // professor ja hr. Sepa kolleeg, 
+				("E+", "Anni Elkin"), // armunud
+				("S+", "Kalle Sepp"), // armunud
+				("JR", "Jenny Harner"), // professor ja hr. Sepa kolleeg, 
 				// oskab rääkida huvitavaid lugusid, kuid on antisimitist
 				("E", "Margaret Elkin"), // väga korrekne, viisakas ja ebahuvitav
 				("M", "kohtunik Master"), // hea kuulaja, äärmiselt taktitundeline
+				("K", "kirikuõpetaja Siidak"), // meeldib anda nõuda
+				("R", "Rabi Simons"), // meeldib vaielda, kuid ta pole kunagi kiuslik
+				("S", "vanaproua Sepp"), // räägib palju, on natuke rumal ja peaaegu kurt
+				("HBV", "hr. Bertini"), // äärmiselt parempoolsete vaadetega
+				("PB", "pr. Bertini"), // lauakombed on väga halvad ja ta kurdab alati
+				("S", "Mari Sepp"), // hellitatud ja rahutu, 
+				// sageli ebaviisakas oma vanemate ja vanaema vastu
+				("VE", "Allan Elkin") // vasakpoolne, on väga huvitatud sotsiaalsetest teemadest
 			};
 
 			Console.WriteLine("Hello World!");
